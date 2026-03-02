3e0f8ffcd967a154cf757c95f0553c543610cace
Michael Wingfield <mwingfie@gmail.com>
2026-03-01 17:40:18 -0700

    go straight to game

--- source/managers/GameManager.hx
+++ source/managers/GameManager.hx
@@ -262,8 +262,6 @@ class GameManager {
 					// needs to force this back to 0
 					nextRoundNumber = currentRoundNumber;
 				}
-				nextStatus = RoundState.STATUS_PRE_ROUND;
-			case RoundState.STATUS_PRE_ROUND:
 				nextStatus = RoundState.STATUS_ACTIVE;
 			case RoundState.STATUS_ACTIVE:
 				nextStatus = RoundState.STATUS_POST_ROUND;
@@ -274,7 +272,7 @@ class GameManager {
 					endGame();
 					return;
 				}
-				nextStatus = RoundState.STATUS_PRE_ROUND;
+				nextStatus = RoundState.STATUS_ACTIVE;
 			case RoundState.STATUS_END_GAME:
 				nextStatus = RoundState.STATUS_LOBBY;
 			case RoundState.STATUS_INACTIVE:
@@ -368,12 +366,6 @@ class GameManager {
 		switch (roundStatus) {
 			case RoundState.STATUS_LOBBY:
 				FlxG.switchState(() -> new LobbyState());
-			case RoundState.STATUS_PRE_ROUND:
-				trace('scooby doo 1');
-				FlxG.switchState(() -> {
-					trace('scooby doo 2');
-					return new PreRoundState();
-				});
 			case RoundState.STATUS_ACTIVE:
 				FlxG.switchState(() -> new PlayState(round));
 			case RoundState.STATUS_POST_ROUND:
